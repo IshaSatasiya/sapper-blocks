e46830780e9a99be92bc700f5a5d7b1971a46ed9
Isha Satasiya <isha@beehively.com>
2023-07-07 15:53:57 +0530

    svelte-index commit

--- saper-svelte/src/routes/index.svelte
+++ saper-svelte/src/routes/index.svelte
@@ -4,6 +4,11 @@
 	function toggle() {
 		user.loggedIn = !user.loggedIn;
 	}
+	let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
 </script>
 
 <style>
@@ -72,4 +77,16 @@
 	<button on:click={toggle}> Log in </button>
 	<p>you are logged in</p>
 {/if}
+
+<h1>The Famous Cats of YouTube</h1>
+
+<ul>
+	{#each cats as { id, name }, i}
+		<li>
+			<a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v={id}">
+				{i + 1}: {name}
+			</a>
+		</li>
+	{/each}
+</ul>
 </div>
