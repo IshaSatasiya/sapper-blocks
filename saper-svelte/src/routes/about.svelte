2860cd507c520f3f14343b1ca4867c6e581a096e
Isha Satasiya <isha@beehively.com>
2023-07-10 10:13:47 +0530

    handler comit

--- saper-svelte/src/routes/about.svelte
+++ saper-svelte/src/routes/about.svelte
@@ -2,6 +2,21 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<script>
+let count = 0
 
-<p>This is the 'about' page. There's not much here.</p>
+function handClick(){
+	count +=1
+}
+
+function newClick(event, stepSize) {
+	count += stepSize
+}
+</script>
+
+<main>
+<button on:click={() => (count = count +1)}>Count {count}</button>
+<button on:click={handClick}>Count {count}</button>
+<button on:click={(event) => newClick(event, 5)}>Count {count}</button>
+<button on:click={(event) => newClick(event, 10)}>Count {count}</button>
+</main>
